4df85d1bc7dc1b43b9523075f13e38c46e7af13c
Alan <alanmak95@gmail.com>
2020-12-16 23:27:57 +0000

    Added a GET request and update tweet list as they are tweeted out

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,49 +3,21 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 $(document).ready(function () {
 
-  // // Gets Tweets From server
-  // const getTweets = () => {
-  //   // Get tweets from URL
-  //   const url = 'http://localhost:8080/tweets';
-
-  //   //Create AJAX request
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: url,
-  //   })
-  //     .then((result) => {
-  //       renderTweets(result);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }
+  // Gets Tweets From server
+  const loadTweets = () => {
+    //Create AJAX request
+    $.ajax({
+      method: 'GET',
+      url: "http://localhost:8080/tweets",
+    })
+      .then((result) => {
+        renderTweets(result);
+      })
+      .catch((err) => console.log("Error", err));
+  }
 
   // Post Tweets to server
   const postTweets = (data) => {
@@ -53,6 +25,9 @@ $(document).ready(function () {
       method: "POST",
       url: "http://localhost:8080/tweets",
       data: data
+    }).then(() => {
+      // Used to update tweet list everytime submit is pressed
+      $('.tweet').replaceWith(loadTweets())
     });
   }
 
@@ -104,6 +79,5 @@ $(document).ready(function () {
     const $newTweetBox = $(this).children('#tweet-text').serialize();
     postTweets($newTweetBox);
   });
-
-  renderTweets(data);
+  loadTweets();
 });
