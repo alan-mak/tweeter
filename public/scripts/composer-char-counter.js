607520973012f2fda830722fa7f28ea457c4fca4
Alan <alanmak95@gmail.com>
2020-12-16 00:45:52 +0000

    Added a hover action over tweeted messages, mentor checked over code for styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
   const $textarea = $('textarea');
   $textarea.on("keyup", function () {
     let counter = $(this).siblings("div").children(".counter")
+
     counter.text(140 - $(this).val().length)
     if ($(this).val().length > 140) {
+      
       counter.addClass("negativeCounter")
     } else {
       counter.removeClass("negativeCounter")
     }
   })
 });
+
+// this.val set as variable
